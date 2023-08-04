78946a730f4c4e1255438d10f436afdfbe135ece
EnzoSani <enzosanabria23@gmail.com>
2023-08-04 17:18:27 -0300

    Create components and service

---
+++ src/app/components/createinvoice/createinvoice.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-createinvoice',
+  templateUrl: './createinvoice.component.html',
+  styleUrls: ['./createinvoice.component.scss']
+})
+export class CreateinvoiceComponent {
+
+}
