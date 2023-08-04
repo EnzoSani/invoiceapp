9599788086772c0534b40edb89b1e63e83ea377e
EnzoSani <enzosanabria23@gmail.com>
2023-08-04 18:00:14 -0300

    Config routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListingComponent } from './components/listing/listing.component';
+import { CreateinvoiceComponent } from './components/createinvoice/createinvoice.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {component:ListingComponent,path:""},
+  {component:CreateinvoiceComponent,path:"createinvoice"},
+  {component:CreateinvoiceComponent,path:"editinvoice/:invoiceno"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
